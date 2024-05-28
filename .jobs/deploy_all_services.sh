51b44308a9ccd5e65d4af7ff2a6a403216fdf537
Prithaj Nath <prithajnath@gmail.com>
2024-05-27 21:31:59 -0400

    don't fail

--- .jobs/deploy_all_services.sh
+++ .jobs/deploy_all_services.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
 
 docker pull iamprithaj/prithaj-portfolio
 docker stop prithaj-portfolio
 docker remove prithaj-portfolio
-docker run -d --name prithaj-portfolio -p 80:9000 -e GH_ACCESS_TOKEN=$GH_ACCESS_TOKEN iamprithaj/prithaj-portfolio
+docker run -d --name prithaj-portfolio -p 80:9000 -e GH_ACCESS_TOKEN="$GH_ACCESS_TOKEN" iamprithaj/prithaj-portfolio
